aaab2a7b09fc31337da72af0f54c420492b0ba0c
caesar <caesaralbertofernandez@gmail.com>
2019-05-15 08:28:18 +0700

    nambah function sum

--- OOP/ViewController.swift
+++ OOP/ViewController.swift
@@ -14,6 +14,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("Updated")
+        print("update lagi gan")
+    }
+    
+    func sum(){
+        
     }
 
 
